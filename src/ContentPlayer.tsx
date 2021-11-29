efda76588095c5c852f6ff4bacd7b51cc271bb6e
Akos Radler <akos.radler@gmail.com>
2021-11-29 17:19:25 +0100

    Enter issue

--- src/ContentPlayer.tsx
+++ src/ContentPlayer.tsx
@@ -1,12 +1,10 @@
 import {
-  AnswerEventProps,
   NavProps,
   Particle,
-  PlayerHandlers,
   PlayerListeners,
   PlayerWrapper,
 } from "@infinitas/app-player";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { Content, ContentSet } from "./model";
 import { ContentUnit } from "./model/ContentUnit";
 import { SlimStampenSuperSubmitButton } from "./SlimStampenSuperSubmitButton";
@@ -16,10 +14,7 @@ export const hasInstructionPage = (item: ContentUnit) =>
   item.contentSets![0].type === "INSTRUCTION";
 
 const Player = ({ item }: any): JSX.Element => {
-  const [externalHandlers, setExternalHandlers] =
-    useState<Omit<PlayerHandlers, "destroy">>();
   const [playerNavigator, setPlayerNavigator] = useState<NavProps>();
-  const [isSubmitted, setIsSubmitted] = useState(false);
   const [isOnInstructionPage, setIsOnInstructionPage] = useState(
     hasInstructionPage(item)
   );
@@ -33,52 +28,11 @@ const Player = ({ item }: any): JSX.Element => {
     if (isOnInstructionPage) {
       setNotInstructionPage();
     }
-    await new Promise((res) => setTimeout(() => res("p1"), 10));
-    setIsSubmitted(() => false);
+
     playerNavigator?.next();
   }, [isOnInstructionPage, playerNavigator, setNotInstructionPage]);
 
   const { generalDrillingItem } = useGeneralDrillingContent(item, 2);
-
-  useEffect(() => {
-    const listener = (event: KeyboardEvent) => {
-      if (
-        (event.code === "Enter" || event.code === "NumpadEnter") &&
-        externalHandlers
-      ) {
-        event.preventDefault();
-        if (isSubmitted || isOnInstructionPage) {
-          handleOnNext();
-        } else if (playerNavigator && generalDrillingItem?.contentSets) {
-          const currentContentSet =
-            generalDrillingItem.contentSets[playerNavigator.current];
-
-          if (currentContentSet?.content) {
-            externalHandlers.submitAnswer(
-              (currentContentSet.content[0].playerPayload as unknown as any).id
-            );
-          }
-        }
-      }
-    };
-    document.addEventListener("keydown", listener);
-    return () => {
-      document.removeEventListener("keydown", listener);
-    };
-  }, [
-    externalHandlers,
-    generalDrillingItem?.contentSets,
-    handleOnNext,
-    isOnInstructionPage,
-    isSubmitted,
-    playerNavigator,
-  ]);
-
-  // This is necessary, in order to sync back the Player's React context to the App's
-  const handleOnSubmitAnswer = async (event: AnswerEventProps) => {
-    console.log(event);
-    setIsSubmitted(() => true);
-  };
 
   const syncNav = (navData: NavProps) => {
     setPlayerNavigator(() => navData);
@@ -86,7 +40,6 @@ const Player = ({ item }: any): JSX.Element => {
 
   const listeners: PlayerListeners = {
     onNext: handleOnNext,
-    onSubmitAnswer: handleOnSubmitAnswer,
   };
 
   const playerData = useMemo(() => {
@@ -112,24 +65,17 @@ const Player = ({ item }: any): JSX.Element => {
           key={"constKey"}
           config={{
             collectData: true,
+            submitButton: null,
+            superSubmitButton: SlimStampenSuperSubmitButton,
             retryConfig: {
               textEntry: 0,
             },
             flattenTopics: false,
           }}
-          setExternalHandlers={setExternalHandlers}
           syncNav={syncNav}
           initialData={playerData}
           listeners={listeners}
         />
-      ) : null}
-      {!isOnInstructionPage ? (
-        <p>
-          Submit and Submit All buttons are both disabled, however when you
-          press enter, the non-existent answer can still be submitted. The Next
-          button is also disabled, but the Player can still be forced to
-          navigate via the enter key.
-        </p>
       ) : null}
     </>
   );
